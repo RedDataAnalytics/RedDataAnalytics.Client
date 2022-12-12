ee92008bcec9841b36246219f6277f7dc4fff410
João Pedro Dante <joaopdante@tryhosting.com.br>
2022-12-12 14:22:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export function test() {
+  let a = JSON.stringify({"data": 1});
+  console.log(a);
+}
+
+test();
+
+/*export async function test2() {
+  let html2canvas = await import("html2canvas");
+  let b = html2canvas.default(document.body);
+}*/
